c04c036a67c546c2062dee804dd3fe68963ee64e
Achim Abeling <achimabeling@web.de>
2018-01-21 17:42:29 +0100

    handles POSTs and logs decibel values

---
+++ README.md
@@ -0,0 +1,7 @@
+Test with
+
+     $ curl -XPOST -H "Content-Type: application/json" -d '{ "decibelValue": 12}' http://localhost:8888/
+
+# installation
+
+
--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 var port = 8888;
 var express = require('express')
+var bodyParser = require("body-parser");
+var cors = require('cors');
+
 var app = express()
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('this should serve some helpful documentation in the future')
 })
+
+app.post('/', function(request,response) {
+
+  console.log("body: ", request.body);
+
+  var decibelValue = request.body.decibelValue;
+  console.log(Date.now() + "," + decibelValue);
+
+  response.json({msg: 'ok'})
+});
 
 app.listen(port, function () {
   console.log('noise-monitoring-server listening on port ' + port)
